8661418eea0cecfa9e1534b1e89dfe7aa13f383a
Crowifick <minecraft4455@live.com>
2013-12-12 22:16:05 -0800

    Finnaly finish config!

--- Modjam_src/net/crowifick/molife/core/config/Config.java
+++ Modjam_src/net/crowifick/molife/core/config/Config.java
@@ -23,19 +23,43 @@ public class Config {
 	}
 	
 	public static void load(Configuration config) {
-		
+
 		try {
-			
+
 			config.load();
-			final Field[] fields Config.class.getField();
+			final Field[] fields = Config.class.getFields();
 			for (final Field field : fields) {
-				
-				
-				
+
+				final CfgID annotation = field.getAnnotation(CfgID.class);
+				if (annotation != null) {
+
+					int id = field.getInt(null);
+					if (annotation.block())
+						id = config.getBlock(field.getName(), id).getInt();
+					else
+						id = config.getItem(field.getName(), id).getInt();
+
+					field.setInt(null, id);
+				}
+
+				else if (field.isAnnotationPresent(CfgBool.class)) {
+
+					boolean bool = field.getBoolean(null);
+					bool = config.get(Configuration.CATEGORY_GENERAL,
+							field.getName(), bool).getBoolean(bool);
+					field.setBoolean(null, bool);
+
+				}
+
 			}
-			
+
+		} catch (final Exception e) {
+
+		} finally {
+
+			config.save();
 		}
-		
+
 	}
-	
+
 }
