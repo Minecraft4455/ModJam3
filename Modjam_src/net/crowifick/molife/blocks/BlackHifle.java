6a1528d6e90653394510359a356cdfce27acafec
Crowifick <minecraft4455@live.com>
2013-12-13 19:39:59 -0800

    Started on Black Hifle

--- Modjam_src/net/crowifick/molife/blocks/BlackHifle.java
+++ Modjam_src/net/crowifick/molife/blocks/BlackHifle.java
@@ -1,14 +1,25 @@
 package net.crowifick.molife.blocks;
 
 import net.minecraft.block.BlockFlower;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.world.World;
 
 public class BlackHifle extends BlockFlower {
 
 	protected BlackHifle(int id) {
+		
 		super(id);
-		
-		
-		
+		this.setCreativeTab(CreativeTabs.tabDecorations);
+        float f = 0.2F;
+		this.setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, f * 3.0F, 0.5F + f);
+        
 	}
+	
+	public boolean canPlaceBlockAt(World par1World, int par2, int par3, int par4) {
+		
+		return super.canPlaceBlockAt(par1World, par2, par3, par4)&& canBlockStay(par1World, par2, par3, par4);
+	
+	}
+
 
 }
